88da8321a7cfcfa4637612f0507acc684302cc00
sprindjack <sprindjack@hotmail.com>
2019-10-04 00:33:00 +0800

    Update index.js

--- cf-worker/index.js
+++ cf-worker/index.js
@@ -3,7 +3,7 @@
 /**
  * static files (404.html, sw.js, conf.js)
  */
-const ASSET_URL = 'https://etherdream.github.io/jsproxy'
+const ASSET_URL = 'https://github.com/EtherDream/jsproxy-browser/tree/master/www'
 
 const JS_VER = 10
 const MAX_RETRY = 1
